f736a04768a60e9f330f653c7e6c5ceca01b0798
srikeshnagoji <36105652+srikeshnagoji@users.noreply.github.com>
2023-09-16 02:47:15 +0530

    Added animations and revamped UX

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { useState } from "react";
 import "./App.css";
 import ChatWindow from "./components/ChatWindow";
 import { NextUIProvider } from "@nextui-org/react";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <NextUIProvider>
       <div className="App">
-        <header className="App-header">
-          <h1 className="logo">Alfred.ai</h1>
-          <h3>Powered by Airbus S&TI</h3>
-        </header>
+        <motion.div
+          className="box"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          <header className="App-header">
+            <h1 className="logo">AIRIES</h1>
+            <h2 className="sub-title">Ink to Insights: LLM for Airbus</h2>
+            <h4>Powered by Airbus S&TI</h4>
+          </header>
+        </motion.div>
+
         <ChatWindow />
       </div>
     </NextUIProvider>
--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Message from "./Message";
 import { fetchResponse } from "../services/ApiService";
-import styled from "styled-components";
+import Typewriter from "typewriter-effect";
 import axios from "axios";
 import "./ChatWindow.css";
 import { Button, ButtonGroup, Textarea, Spacer } from "@nextui-org/react";
-import { Card, CardBody, Avatar } from "@nextui-org/react";
+import { Card, CardBody, Avatar, Skeleton } from "@nextui-org/react";
 import { Accordion, AccordionItem, Divider } from "@nextui-org/react";
+import { motion } from "framer-motion";
+import { ScrollShadow } from "@nextui-org/react";
 
 //   background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
 
@@ -24,7 +26,8 @@ const ChatWindow = () => {
       let bodyFormData = new FormData();
       bodyFormData.append("user_prompt", userInput);
       const response = await axios.post(
-        "http://localhost:5110/api/prompt_route",
+        "http://localhost:5110/api/prompt_route", //actual
+        // "http://localhost:3130/api/prompt_route", //temp
         bodyFormData
       );
       const newMessage = {
@@ -52,80 +55,136 @@ const ChatWindow = () => {
   return (
     <>
       <div className="ChatContainer">
-        <div className="MessagesContainer">
+        {/* <div className="MessagesContainer"> */}
+        <ScrollShadow className="snap-y w-[300px] h-[400px] MessagesContainer">
+          {messages.length === 0 && (
+            <div className=" snap-center typewriter">
+              <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 1.6,
+                  delay: 1.3,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+              >
+                <Typewriter
+                  options={{
+                    strings: [
+                      "What is mach?",
+                      "Tell me about Airbus.",
+                      "what are the materials used in an aircraft?",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    delay: 0.01,
+                  }}
+                />
+              </motion.div>
+            </div>
+          )}
           {messages.map((message, index) => (
             <div
               style={{
                 padding: "1em",
               }}
             >
-              <Card key={"card" + index}>
-                <CardBody className="flex-col gap-3 items-center">
+              <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.8,
+                  delay: 0.5,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+              >
+                <Card key={"card" + index}>
                   <CardBody className="flex-col gap-3 items-center">
-                    <CardBody className="flex-row gap-3 items-center">
-                      <Avatar
-                        name="You"
-                        size="md"
-                        key={"you" + index}
-                        className="shrink-0"
-                      />
-                      <p>{message.prompt}</p>
-                    </CardBody>
+                    <CardBody className="flex-col gap-3 items-center">
+                      <CardBody className="flex-row gap-3 items-center">
+                        <Avatar
+                          name="You"
+                          size="md"
+                          key={"you" + index}
+                          className="shrink-0"
+                          isBordered
+                          color="secondary"
+                        />
+                        <p>{message.prompt}</p>
+                      </CardBody>
 
-                    <CardBody className="flex-row gap-3 items-center">
-                      <Avatar
-                        name="Alfred"
-                        size="md"
-                        key={"Alfred" + index}
-                        className="shrink-0"
-                      />
-                      <p>{message.answer}</p>
-                    </CardBody>
+                      <CardBody className="flex-row gap-3 items-center">
+                        <Avatar
+                          name="AIRIES"
+                          size="md"
+                          key={"AIRIES" + index}
+                          className="shrink-0"
+                          isBordered
+                          color="primary"
+                        />
+                        <p>{message.answer}</p>
+                      </CardBody>
 
-                    <Divider />
+                      <Divider />
+                    </CardBody>
+                    <p>Sources:</p>
+                    <Accordion isCompact>
+                      {message.metadata.map((obj, i) => (
+                        <AccordionItem
+                          key={index + i}
+                          // aria-label="Accordion 1"
+                          title={obj[1].substr(0, 30) + "..."}
+                          subtitle={<span>{obj[0]}</span>}
+                        >
+                          {obj[1]}
+                        </AccordionItem>
+                      ))}
+                    </Accordion>
                   </CardBody>
-                  <p>Sources:</p>
-                  <Accordion isCompact>
-                    {message.metadata.map((obj, i) => (
-                      <AccordionItem
-                        key={index + i}
-                        // aria-label="Accordion 1"
-                        title={obj[1].substr(0, 30) + "..."}
-                        subtitle={<span>{obj[0]}</span>}
-                      >
-                        {obj[1]}
-                      </AccordionItem>
-                    ))}
-                  </Accordion>
-                </CardBody>
-              </Card>
+                </Card>
+              </motion.div>
               {/* <Spacer x={12} /> */}
             </div>
           ))}
+        </ScrollShadow>
+        {/* </div> */}
+      </div>
+      <motion.div
+        className="box"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 1.3,
+          delay: 1,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <div className="UserInputContainer sticky top-0 z-50">
+          <Textarea
+            //   label="Description"
+            labelPlacement="outside"
+            placeholder="Type your message..."
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            onKeyPress={handleKeyPress}
+            fullWidth={true}
+            size="lg"
+            classNames="w-5/6"
+          />
+          <Spacer x={5} />
+          <Button
+            color="primary"
+            isLoading={isLoading}
+            onClick={handleUserMessage}
+            size="lg"
+            className={"w-40 shrink-0"}
+          >
+            {isLoading ? "Processing..." : "Send"}
+          </Button>
         </div>
-      </div>
-      <div className="UserInputContainer sticky top-0 z-50">
-        <Textarea
-          //   label="Description"
-          labelPlacement="outside"
-          placeholder="Type your message..."
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          onKeyPress={handleKeyPress}
-          fullWidth={true}
-          size="lg"
-          classNames="w-5/6"
-        />
-        <Spacer x={5} />
-        <Button
-          color="primary"
-          isLoading={isLoading}
-          onClick={handleUserMessage}
-          className="w-40 shrink-0"
-        >
-          {isLoading ? "Processing..." : "Send"}
-        </Button>
-      </div>
+      </motion.div>
     </>
   );
 };
